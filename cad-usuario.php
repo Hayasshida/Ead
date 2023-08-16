8d95d1e8fd3af49989ee545c47d8ed69784a5b6a
Hayasshida <pedrohayashida9@gmail.com>
2023-08-16 08:46:22 -0300

    primeiro commit

---
+++ cad-usuario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EAD - Novo Usuário</title>
+    <link rel="stylesheet" href="css/cad.css">
+</head>
+<body>
+    <div id="main">
+        <section>
+            <h2>EAD Novo Usuário </h2>
+            <form action="#">
+                <input type="text" name="usuario" class="campo" placeholder="Usuário" required>
+                <input type="text" name="email" class="campo" placeholder="E-mail" required>
+                <input type="password" name="senha" class="campo" placeholder="Senha" required>
+                <input type="password" name="senha2" class="campo" placeholder="Redigite a Senha" required>
+                <div class="cont-button"><button>Cadastre-se</button></div>
+            </form>
+            <a href="index.php" target="_self">Voltar</a>
+        </section>
+    </div>
+
+</body>
+</html>
